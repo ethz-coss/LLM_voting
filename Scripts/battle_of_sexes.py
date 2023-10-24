fe8d8c9b2e2377e79545cc72cd09326c014ad738
mbkorecki <mbokorecki@gmail.com>
2023-10-24 16:59:18 +0200

    smol fix

--- Scripts/battle_of_sexes.py
+++ Scripts/battle_of_sexes.py
@@ -14,15 +14,17 @@ def run_game():
     """
     
     # Create an agent with id 0, recall 10 and initial context
-    init_message = Message(time=0, content="You are playing a game repeatedly with another player. In this game, you can choose between Option J and Option F. You will play 10 rounds in total with the same player. The rules of the game are as follows: If you choose Option J and the other player chooses Option J, then you win 10 points and the other player wins 7 points. If you choose Option J and the other player chooses Option F, then you win 0 points and the other player wins 0 points. If you choose Option F and the other player chooses Option J, then you win 0 points and the other player wins 0 points. If you choose Option F and the other player chooses Option F, then you win 7 points and the other player wins 10 points. Say only the letter corresponding to your action and no other tokens. You must always give an answer. You want to get as much points as possible.", role="system")
+    init_message = Message(time=0, content="You are playing a game repeatedly with another player. In this game, you can choose between Option J and Option F. You will play 10 rounds in total with the same player. The rules of the game are as follows: If you choose Option J and the other player chooses Option J, then you win many points and the other player wins medium points. If you choose Option J and the other player chooses Option F, then you win no points and the other player wins no points. If you choose Option F and the other player chooses Option J, then you win no points and the other player wins no points. If you choose Option F and the other player chooses Option F, then you win medium points and the other player wins no points. Say only the letter corresponding to your action and no other tokens. You must always give an answer. You want to get as much points as possible.", role="system")
     a = agent.Agent(aid=0, recall=0, initial_context=init_message)
     
     init_message = Message(time=0, content="You are playing a game repeatedly with another player. In this game, you can choose between Option J and Option F. You will play 10 rounds in total with the same player. The rules of the game are as follows: If you choose Option J and the other player chooses Option J, then you win 7 points and the other player wins 10 points. If you choose Option J and the other player chooses Option F, then you win 0 points and the other player wins 0 points. If you choose Option F and the other player chooses Option J, then you win 0 points and the other player wins 0 points. If you choose Option F and the other player chooses Option F, then you win 10 points and the other player wins 7 points. Say only the letter corresponding to your action. You must always give an answer.", role="system")
     b = agent.Agent(aid=0, recall=0, initial_context=init_message)
 
 
-    scores = {"JJ":[10, 7], "JF":[0, 0], "FJ":[0, 0], "FF":[7, 10]}
-    
+    scores = {"JJ":[10, 7], "JF":[0, 0], "FJ":[0, 0], "FF":[7, 10]}    
+
+    # scores = {"JJ":["many", "medium"], "JF":["no", "no"], "FJ":["no", "many"], "FF":["medium", "many"]}
+
     m1 = Message(time=0, content="You are currently playing round 1. Q: Which Option do you choose, Option J or Option F?", role="user")
     print(m1)
     r1 = a.perceive(message=m1, max_tokens=1)
@@ -33,7 +35,7 @@ def run_game():
     score1, score2 = scores[r1.content + r2.content]
 
     for i in range(1, 10):
-        m1 = Message(time=i, content="In round " + str(i) + " you chose Option " + r1.content + " and the other player chose Option " + "F" + ". Thus, you won " + str(score1) + " points and the other player won " + str(score2) + " points You are currently playing round " + str(i+1) + ". Q: Which Option do you choose, Option J or Option F?", role="user")
+        m1 = Message(time=i, content="In round " + str(i) + " you chose Option " + r1.content + " and the other player chose Option " + r2.content + ". Thus, you won " + str(score1) + " points and the other player won " + str(score2) + " points You are currently playing round " + str(i+1) + ". Q: Which Option do you choose, Option J or Option F?", role="user")
         m2 = Message(time=i, content="In round " + str(i) + " you chose Option " + r2.content + " and the other player chose Option " + r1.content + ". Thus, you won " + str(score2) + " points and the other player won " + str(score1) + " points. You are currently playing round " + str(i+1) + ". Q: Which Option do you choose, Option J or Option F?", role="user")
 
         print(m1)
